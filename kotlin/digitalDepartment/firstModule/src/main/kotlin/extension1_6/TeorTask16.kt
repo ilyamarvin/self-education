cb446d218a7c8ea96597e4dee17823a014a3e15f
IlyaMarvin <ilya.sereda2002@mail.ru>
2022-11-02 01:25:04 +0300

    Complete 1.6 module

--- kotlin/digitalDepartment/firstModule/src/main/kotlin/extension1_6/TeorTask16.kt
+++ kotlin/digitalDepartment/firstModule/src/main/kotlin/extension1_6/TeorTask16.kt
@@ -1,17 +1,27 @@
 package extension1_6
 
-fun String.isValidCCNumber() {
-
+fun String.isValidCCNumber(): Boolean {
+    var card = this.replace(" ", "")
+    var sum = 0
+    card = card.reversed()
+    for (i in card.indices) {
+        sum += when {
+            (i % 2 != 0 && (card[i].digitToInt()*2 <= 9)) -> (card[i].digitToInt())*2
+            (i % 2 != 0 && (card[i].digitToInt()*2 > 9)) -> (card[i].digitToInt())*2-9
+            else -> card[i].digitToInt()
+        }
+    }
+    return sum % 10 == 0
 }
 
 fun IntArray.isSorted(): Boolean {
     for (i in 1..this.size)
-        if (this[i] < this[i+1])
+        if (this[i + 1] > this[i])
             return false
     return true
 }
 
 fun main() {
-    val array:IntArray = intArrayOf(10, 15, 20)
+    val array: IntArray = intArrayOf(10, 15, 20)
     println(array.isSorted())
 }
